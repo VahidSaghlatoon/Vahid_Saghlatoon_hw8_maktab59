d54183064b796ef10b7d45435c9a18ad2b7de4db
vahid <vahid.d.w@gmail.com>
2021-09-27 16:14:30 +0330

    add two new object from lamp and car

--- src/main/java/question1/DemoTurners.java
+++ src/main/java/question1/DemoTurners.java
@@ -2,13 +2,20 @@ package question1;
 
 public class DemoTurners {
     public static void main(String[] args) {
+
         Turner leaf = new Leaf();
         leaf.turn();
+
         Turner page = new Page();
         page.turn();
+
         Turner pancake = new Pancake();
         pancake.turn();
+
         Turner lamp = new Lamp();
         lamp.turn();
+
+        Turner car = new Car();
+        car.turn();
     }
 }
