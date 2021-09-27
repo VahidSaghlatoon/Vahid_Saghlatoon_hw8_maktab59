d1575772cdecaa07529dacbad2fdcbd5f8400adf
vahid <vahid.d.w@gmail.com>
2021-09-27 15:47:55 +0330

    in class leaf declare color property with setter and getter and implements turn method

--- src/main/java/question1/Leaf.java
+++ src/main/java/question1/Leaf.java
@@ -1,4 +1,19 @@
 package question1;
 
-public class Leaf {
+public class Leaf implements Turner {
+    private String color ;
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    @Override
+    public void turn() {
+        System.out.println("new color is : " + getColor());
+
+    }
 }
