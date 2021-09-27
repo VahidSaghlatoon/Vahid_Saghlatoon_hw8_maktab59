9c3bcc8a9881ff4833813235c7158db243e6c026
vahid <vahid.d.w@gmail.com>
2021-09-27 15:57:17 +0330

    create class DemoTurners

---
+++ src/main/java/question1/DemoTurners.java
@@ -0,0 +1,12 @@
+package question1;
+
+public class DemoTurners {
+    public static void main(String[] args) {
+        Turner leaf = new Leaf();
+        leaf.turn();
+        Turner page = new Page();
+        page.turn();
+        Turner pancake = new Pancake();
+        pancake.turn();
+    }
+}
