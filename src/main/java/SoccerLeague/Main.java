7e2ed6e06f71c9678c2bdb80dd501d69aab8c5d2
vahid <vahid.d.w@gmail.com>
2021-09-29 22:07:24 +0330

    change menu method

--- src/main/java/SoccerLeague/Main.java
+++ src/main/java/SoccerLeague/Main.java
@@ -1,17 +1,21 @@
 package SoccerLeague;
 
+import SoccerLeague.dao.ResultDao;
 import SoccerLeague.service.CoachService;
 import SoccerLeague.service.PlayerService;
+import SoccerLeague.service.ResultService;
 import SoccerLeague.service.TeamService;
 
 import java.util.Scanner;
 
 public class Main {
 
-    private  Scanner input = new Scanner(System.in);
-    private PlayerService  playerService = new PlayerService();
+    private Scanner input = new Scanner(System.in);
+
+    private PlayerService playerService = new PlayerService();
     private CoachService coachService = new CoachService();
     private TeamService teamService = new TeamService();
+    private ResultService resultService = new ResultService();
 
 
     public static void main(String[] args) {
@@ -19,29 +23,37 @@ public class Main {
         Main test = new Main();
         test.menu();
 
+
+
     }
-    public   void menu(){
+
+
+    public void menu() {
         boolean check = true;
         while (check) {
             System.out.print("1--> coach max with max salary\n" +
                     "2--> player max salary by season\n" +
                     "3--> city team count\n" +
-                    "0--> exit\n"+
+                    "4--> team by  point\n" +
+                    "0--> exit\n" +
                     "--> ");
             int choice = input.nextInt();
 
-            switch (choice){
-                case 1 :
+            switch (choice) {
+                case 1:
                     coachService.maxPrice();
                     break;
-                case 2 :
+                case 2:
                     playerService.maxPriceBySeason();
                     break;
-                case 3 :
+                case 3:
                     teamService.cityTeamCount();
                     break;
-                case 0 :
-                    check = false ;
+                case 4:
+                    resultService.readBypoint();
+                    break;
+                case 0:
+                    check = false;
                     break;
 
             }
