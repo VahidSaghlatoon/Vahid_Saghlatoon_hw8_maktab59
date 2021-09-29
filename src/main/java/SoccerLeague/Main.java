f90bc0b6ff231f3e3af93b8a7e9fef0db23035cf
vahid <vahid.d.w@gmail.com>
2021-09-29 21:12:10 +0330

    create menu method

--- src/main/java/SoccerLeague/Main.java
+++ src/main/java/SoccerLeague/Main.java
@@ -1,13 +1,50 @@
 package SoccerLeague;
 
+import SoccerLeague.service.CoachService;
+import SoccerLeague.service.PlayerService;
 import SoccerLeague.service.TeamService;
 
-public class Main {
-    public static void main(String[] args) {
-//        CoachService coachService = new CoachService();
-//        coachService.maxPrice();
+import java.util.Scanner;
 
-        TeamService teamService = new TeamService();
-        teamService.cityTeamCount();
+public class Main {
+
+    private  Scanner input = new Scanner(System.in);
+    private PlayerService  playerService = new PlayerService();
+    private CoachService coachService = new CoachService();
+    private TeamService teamService = new TeamService();
+
+
+    public static void main(String[] args) {
+
+        Main test = new Main();
+        test.menu();
+
+    }
+    public   void menu(){
+        boolean check = true;
+        while (check) {
+            System.out.print("1--> coach max with max salary\n" +
+                    "2--> player max salary by season\n" +
+                    "3--> city team count\n" +
+                    "0--> exit\n"+
+                    "--> ");
+            int choice = input.nextInt();
+
+            switch (choice){
+                case 1 :
+                    coachService.maxPrice();
+                    break;
+                case 2 :
+                    playerService.maxPriceBySeason();
+                    break;
+                case 3 :
+                    teamService.cityTeamCount();
+                    break;
+                case 0 :
+                    check = false ;
+                    break;
+
+            }
+        }
     }
 }
